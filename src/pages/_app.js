af6bbb986dfde2d805b1d6b7c5af43efd3ec823c
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2025-04-15 12:38:46 -0600

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,10 +6,12 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Paraíso Huatulco App</title>
-        <meta name="description" content="Paraíso Huatulco"/>
-        <link rel="manifest" href="/manifest.json"/>
-        <meta name="theme-color" content="#fff"/>
-        <link rel="apple-touch-icon" href="/icon512_rounded.png"/>
+        <meta name="description" content="Paraíso Huatulco" />
+        <meta name="theme-color" content="#fcd847" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon512_rounded.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
       </Head>
       <Component {...pageProps} />
     </>
