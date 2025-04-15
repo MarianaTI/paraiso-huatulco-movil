99c1ecb2ca1c80a5ada2bc6e7c5c964b93e046d0
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2025-04-15 12:26:32 -0600

    feat(): manifest

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Paraíso Huatulco App</title>
+        <meta name="description" content="Paraíso Huatulco"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <meta name="theme-color" content="#fff"/>
+        <link rel="apple-touch-icon" href="/icon512_rounded.png"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
