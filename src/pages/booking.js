e1785461689f43d25f769aefac582a6931f38301
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2025-07-11 13:27:21 -0600

    feat(): datos de tipo servicio 2 y 3

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -4,52 +4,64 @@ import useOnlineStatus from "@/hooks/useOnlineStatus";
 import { saveOfflineBooking } from "@/utils/offlineBooking";
 import { sendBooking } from "@/utils/sendBooking";
 import useRatesPrice from "@/hooks/useRatesPrice";
+import { useSelector } from "react-redux";
 
 export default function Booking() {
   const router = useRouter();
   const isOnline = useOnlineStatus();
+  const userId = useSelector((state) => state.user._id);
 
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(2);
   const [rent, setRent] = useState([]);
   const [product, setProduct] = useState([]);
   const [rate, setRate] = useState(null);
-  const [adultos, setAdultos] = useState(1);
+  const [adultos, setAdultos] = useState(2);
   const [menores, setMenores] = useState(0);
 
   const ratesData = useRatesPrice(rate, adultos, menores);
 
+  console.log("Código de producto", product);
+
   const [data, setData] = useState({
-    limit_payment: "2025-04-22",
-    limit_customer: "2025-04-22",
-    rate_code: "1",
-    product_code: product.product_code,
-    total: "0",
+    alias: "",
+    categoria_transporte: "",
+    categoria_traslado: "",
+    client_lastname: "",
+    client_mail: "",
+    client_name: "",
+    client_phone: "",
+    codigo: "",
+    comments: "",
     currency: "MXN",
-    pax_menor: "0",
-    infantes: "0",
-    pagado: "0",
+    destino: "HX",
+    habitacion: "",
+    hora: "09:00:00",
     hora_llegada: "",
     hora_salida: "",
+    hotel: "Bahía Huatulco",
+    id_dealer: "",
+    id_proveedor: "",
+    id_servicio: "",
+    id_usuario: "",
+    infantes: "0",
+    limit_customer: "2025-04-22",
+    limit_payment: "INMEDIATO",
+    nombre_producto: "",
     numero_vuelo: "",
     numero_vuelo_salida: "",
-    hotel: "Bahía Huatulco",
-    categoria_transporte: "1",
-    tipo_viaje: "",
-    id_usuario: "2",
-    destino: "HX",
-    categoria_traslado: "semiprivado",
+    pagado: "0",
+    pax_adults: "2",
+    pax_menor: "0",
     pickup: "08:40",
     pickup_salida: "",
-    codigo: "",
-    hora: "09:00:00",
     plataforma: "movil",
-    client_name: "test pwa",
-    client_lastname: "movil",
-    client_phone: "9999999999",
-    client_mail: "test@pwa.com",
+    product_code: "",
+    rate_code: "",
     start_date: "2025-07-02",
-    pax_adults: "2",
-    comments: "prueba de envío con pwa",
+    tipo_viaje: "",
+    total: "0",
+    vendedor_referenciado: "",
+    zona_hotel: "",
   });
 
   useEffect(() => {
@@ -99,7 +111,53 @@ export default function Booking() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    const bookingData = { ...data };
+
+    let categoria_traslado = "";
+
+    if (rate.service_type == 1) {
+      categoria_traslado = "compartido";
+    } else if (rate.service_type == 2) {
+      categoria_traslado = "privado";
+    } else if (rate.service_type == 3) {
+      categoria_traslado = "semiprivado";
+    }
+
+    const startDate = new Date(data.start_date);
+    const today = new Date();
+
+    const tentativeLimit = new Date(startDate);
+    tentativeLimit.setDate(startDate.getDate() - 7);
+
+    const formatDate = (date) => {
+      return date.toISOString().split("T")[0];
+    };
+
+    let limit_payment = "";
+    let limit_customer = "";
+
+    if (tentativeLimit < today) {
+      limit_payment = formatDate(today);
+      limit_customer = formatDate(today);
+    } else {
+      limit_payment = formatDate(tentativeLimit);
+      limit_customer = formatDate(tentativeLimit);
+    }
+
+    const bookingData = {
+      ...data,
+      categoria_traslado,
+      product_code: product.product_code,
+      // id_producto: product.product_code,
+      // servicio: product.name,
+      destino: product.codigo_destino,
+      id_servicio: product.id_servicio,
+      limit_payment,
+      limit_customer,
+      pax_adults: String(adultos),
+      pax_menor: String(menores),
+      id_usuario: userId,
+      rate_code: rate.rate_code,
+    };
 
     if ("Notification" in window && Notification.permission !== "granted") {
       const permission = await Notification.requestPermission();
@@ -132,33 +190,31 @@ export default function Booking() {
     }
 
     try {
-      const response = await sendBooking(bookingData);
-      setRent(response);
-      if (
-        "serviceWorker" in navigator &&
-        "Notification" in window &&
-        Notification.permission === "granted"
-      ) {
-        const reg = await navigator.serviceWorker.ready;
-        reg.showNotification("Reserva enviada", {
-          body: "Tu reserva fue registrada con éxito.",
-          icon: "/icon512_rounded.png",
-        });
-      }
-      router.push("/home");
+      console.log("Reserva: ", bookingData);
+
+      // const response = await sendBooking(bookingData);
+      // setRent(response);
+      // if (
+      //   "serviceWorker" in navigator &&
+      //   "Notification" in window &&
+      //   Notification.permission === "granted"
+      // ) {
+      //   const reg = await navigator.serviceWorker.ready;
+      //   reg.showNotification("Reserva enviada", {
+      //     body: "Tu reserva fue registrada con éxito.",
+      //     icon: "/icon512_rounded.png",
+      //   });
+      // }
+      // router.push("/home");
     } catch (error) {
       console.log("Error en submit", error);
     }
   };
 
-  const stepTitles = [
-    "DATOS",
-    "DETALLES",
-    "PAGO",
-  ];
+  const stepTitles = ["DATOS", "DETALLES", "PAGO"];
 
   return (
-    <div className="mt-4">
+    <div className="pt-4">
       <div className="px-4 py-2">
         <h1 className="booking-title">RESERVA</h1>
         {/* <p className="booking-subtitle">Completa este formulario para crear una nueva reservación.</p> */}
@@ -231,95 +287,104 @@ export default function Booking() {
         {step === 2 && (
           <section className="booking-steps-content">
             <h3>Información de servicio</h3>
-            <label className="form-label-styled">Lugar del hospedaje</label>
-            <input
-              type="text"
-              name="hotel"
-              value={data.hotel}
-              onChange={handleChange}
-              className="mb-3 form-input-styled"
-            />
-            <label className="form-label-styled">Fecha de inicio</label>
-            <input
-              type="date"
-              name="start_date"
-              value={data.start_date}
-              onChange={handleChange}
-              className="mb-3 form-input-styled"
-            />
-            <div className="grid-form">
-              <div className="grid-item">
-                <label className="form-label-styled">Horario de tour</label>
+            {(product.id_servicio === "2" || product.id_servicio === "3") && (
+              <>
+                <label className="form-label-styled">Lugar del hospedaje</label>
                 <input
-                  type="time"
-                  name="hora_llegada"
-                  value={data.hora_llegada}
+                  type="text"
+                  name="hotel"
+                  value={data.hotel}
                   onChange={handleChange}
-                  className="mb-2 form-input-styled"
+                  className="mb-3 form-input-styled"
                 />
-              </div>
-              <div className="grid-item">
-                <label className="form-label-styled">Pickup</label>
+                <label className="form-label-styled">Fecha de inicio</label>
                 <input
-                  type="time"
-                  name="pickup"
-                  value={data.pickup}
+                  type="date"
+                  name="start_date"
+                  value={data.start_date}
                   onChange={handleChange}
-                  className="mb-2 form-input-styled"
+                  className="mb-3 form-input-styled"
                 />
-              </div>
-              <div className="grid-item">
-                <label className="form-label-styled">Adultos</label>
-                <input
-                  type="number"
-                  min="0"
-                  value={String(adultos)}
-                  onChange={(e) => handleNumericChange(e, setAdultos)}
-                  onFocus={(e) => e.target.select()}
-                  className="mb-2 form-input-styled"
-                />
-              </div>
-              <div className="grid-item">
-                <label className="form-label-styled">Menores</label>
-                <input
-                  type="number"
-                  min="0"
-                  value={String(menores)}
-                  onChange={(e) => handleNumericChange(e, setMenores)}
-                  onFocus={(e) => e.target.select()}
-                  className="mb-2 form-input-styled"
-                />
-              </div>
-              <div className="grid-item">
-                <label className="form-label-styled">Infantes</label>
-                <input
-                  type="number"
-                  name="infantes"
-                  value={data.infantes}
-                  onChange={handleChange}
-                  className="mb-2 form-input-styled"
-                />
-              </div>
-            </div>
-            <div className="grid-form mb-3">
-              <button
-                className="booking-button-cancel"
-                onClick={() => setStep(1)}
-              >
-                Atrás
-              </button>
-              <button className="booking-button" onClick={() => setStep(3)}>
-                Continuar
-              </button>
-            </div>
+                <div className="grid-form">
+                  <div className="grid-item">
+                    <label className="form-label-styled">Horario de tour</label>
+                    <input
+                      type="time"
+                      name="hora_llegada"
+                      value={data.hora_llegada}
+                      onChange={handleChange}
+                      className="mb-2 form-input-styled"
+                    />
+                  </div>
+                  <div className="grid-item">
+                    <label className="form-label-styled">Pickup</label>
+                    <input
+                      type="time"
+                      name="pickup"
+                      value={data.pickup}
+                      onChange={handleChange}
+                      className="mb-2 form-input-styled"
+                    />
+                  </div>
+                  <div className="grid-item">
+                    <label className="form-label-styled">Adultos</label>
+                    <input
+                      type="number"
+                      min="2"
+                      value={String(adultos)}
+                      onChange={(e) => handleNumericChange(e, setAdultos)}
+                      onFocus={(e) => e.target.select()}
+                      className="mb-2 form-input-styled"
+                    />
+                  </div>
+                  <div className="grid-item">
+                    <label className="form-label-styled">Menores</label>
+                    <input
+                      type="number"
+                      min="0"
+                      value={String(menores)}
+                      onChange={(e) => handleNumericChange(e, setMenores)}
+                      onFocus={(e) => e.target.select()}
+                      className="mb-2 form-input-styled"
+                    />
+                  </div>
+                  <div className="grid-item">
+                    <label className="form-label-styled">Infantes</label>
+                    <input
+                      type="number"
+                      name="infantes"
+                      value={data.infantes}
+                      onChange={handleChange}
+                      className="mb-2 form-input-styled"
+                    />
+                  </div>
+                </div>
+                <div className="grid-form mb-3">
+                  <button
+                    className="booking-button-cancel"
+                    onClick={() => setStep(1)}
+                  >
+                    Atrás
+                  </button>
+                  <button className="booking-button" onClick={() => setStep(3)}>
+                    Continuar
+                  </button>
+                </div>
+              </>
+            )}
+            {product.id_servicio === "1" && (
+              <>
+                <label>Traslado</label>
+              </>
+            )}
           </section>
         )}
         {step === 3 && (
           <section className="booking-steps-content">
             <h3>Información de tarifa</h3>
-            <p style={{fontSize: 17}}>Tarifa de tipo {rate?.rate_title}</p>
+            <p style={{ fontSize: 17 }}>Tarifa de tipo {rate?.rate_title}</p>
             {ratesData && (
-              <p style={{fontSize: 17}}>
+              <p style={{ fontSize: 17 }}>
                 Total:{" "}
                 <span className="rate-price">
                   ${parseFloat(ratesData.total).toFixed(2)} {rate?.moneda}
