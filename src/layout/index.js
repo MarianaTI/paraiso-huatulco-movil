e48e2df9a2dcf6f60a44e84d0b5cba5dfc6898b0
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2025-07-15 16:01:37 -0600

    feat(): rutas protegidas

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,15 +1,48 @@
 import Navbar from "@/components/navbar/Navbar";
+import Cookies from "js-cookie";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Layout = ({ children }) => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
   const noNavbar = router.pathname.match(/^\/$/);
+
+  useEffect(() => {
+    const publicRoutes = ["/"];
+    const isPublic = publicRoutes.includes(router.pathname);
+
+    const userCookie = Cookies.get("user_session");
+
+    if (!userCookie && !isPublic) {
+      router.push("/");
+    } else {
+      setLoading(false);
+    }
+  }, [router.pathname]);
+
+  if (loading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="loader-container">
+          <div class="loader"></div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
-      {!noNavbar && <Navbar/>}
-      <div style={{backgroundColor: "#f8f8f8", minHeight: "calc(100vh - 72.75px)"}}>{children}</div>
+      {!noNavbar && <Navbar />}
+      <div
+        style={{
+          backgroundColor: "#f8f8f8",
+          minHeight: "calc(100vh - 72.75px)",
+        }}
+      >
+        {children}
+      </div>
     </div>
   );
 };
