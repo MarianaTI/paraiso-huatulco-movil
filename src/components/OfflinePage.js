c5f6c8fd35d9aaaf859c7b5a001442acc298cab5
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2025-07-21 12:21:13 -0600

    changes

---
+++ src/components/OfflinePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OfflinePage = () => {
+    return (
+        <div className='offline-page-container'>
+            <h1>Conexión requerida</h1>
+            <span className='offline-page-description'>Para acceder a esta funcionalidad necesitas estar conectado a internet. Revisa tu red e inténtalo nuevamente.</span>
+        </div>
+    );
+}
+
+export default OfflinePage;
---
+++ src/pages/offline.js
@@ -0,0 +1,10 @@
+import OfflinePage from '@/components/OfflinePage'
+import React from 'react'
+
+export default function Ejemplo() {
+  return (
+    <div>
+        <OfflinePage/>
+    </div>
+  )
+}
