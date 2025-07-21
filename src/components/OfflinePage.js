b16e7787fe859fd9261431301753ec90e8656b01
Mariana Trujillo Islas <105942792+MarianaTI@users.noreply.github.com>
2025-07-21 17:17:01 -0600

    feat(): empty state

--- src/components/OfflinePage.js
+++ src/components/OfflinePage.js
@@ -1,9 +1,11 @@
+import Image from 'next/image';
 import React from 'react';
 
 const OfflinePage = () => {
     return (
         <div className='offline-page-container'>
-            <h1>Conexión requerida</h1>
+            <Image src="/empty-state.png" alt='empty-state-network' width={250} height={200}/>
+            <h1 className='mt-5 offline-page-title'>Conexión requerida</h1>
             <span className='offline-page-description'>Para acceder a esta funcionalidad necesitas estar conectado a internet. Revisa tu red e inténtalo nuevamente.</span>
         </div>
     );
